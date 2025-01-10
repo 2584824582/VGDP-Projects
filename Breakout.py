546d71bc21fcc03cae1ece095fd500a86010e0ca
2584824582 <81043855+2584824582@users.noreply.github.com>
2025-01-10 08:57:02 -0500

    Breakout.py

--- Breakout.py
+++ Breakout.py
@@ -20,7 +20,7 @@ BLUE = (0, 0, 255)
 PURPLE = (128, 0, 128)
 
 #Variables
-speed = 5
+speed = 7
 clock = pygame.time.Clock()
 paddle = pygame.Rect(350, 500, 100, 20)
 
@@ -35,17 +35,27 @@ BLOCK_COLUMNS = 10
 blocks = []
 
 #Lives Text
-def lives():
-    font1 = pygame.font.Font(None, 48)
-    text1 = font1.render("Lives: ", True, BLACK)
-    text_rect1 = text1.get_rect(center = (75, 570))
-    screen.blit(text1, text_rect1)
-    
-    font2 = pygame.font.Font(None, 48)
-    text2 = font2.render(str(playerlives), True, BLACK)
-    text_rect2 = text2.get_rect(center = (135, 570))
-    screen.blit(text2, text_rect2)
-
+def livesText():
+  global playerlives
+  font1 = pygame.font.Font(None, 48)
+  text1 = font1.render("Lives: ", True, BLACK)
+  text_rect1 = text1.get_rect(center = (75, 570))
+  screen.blit(text1, text_rect1)
+  
+  font2 = pygame.font.Font(None, 48)
+  text2 = font2.render(str(playerlives), True, BLACK)
+  text_rect2 = text2.get_rect(center = (135, 570))
+  screen.blit(text2, text_rect2)
+def endtextLose():
+  font3 = pygame.font.Font(None, 48)
+  text3 = font3.render("You Lose!", True, BLACK)
+  text_rect3 = text3.get_rect(center = (395, 295))
+  screen.blit(text3, text_rect3)
+def endtextWin():
+  font4 = pygame.font.Font(None, 48)
+  text4 = font4.render("You Win!!", True, BLACK)
+  text_rect4 = text4.get_rect(center = (395, 295))
+  screen.blit(text4, text_rect4)
 #Create the list
 for row in range(BLOCK_ROWS):
   for col in range(BLOCK_COLUMNS):
@@ -55,35 +65,65 @@ for row in range(BLOCK_ROWS):
     blocks.append(block)
 
 #Draw Ball
-ball_speed = [5, 5]
+ball_speed = [7, 7]
 
 ball_rect = pygame.Rect(395, 295, 20, 10)
 
 #Ball Movement
+def reset_ball():
+  global straight
+  ball_rect.center = (395, 295)
+  straight = True
 def ball_movement_straight():
   global ball_speed
   global straight
+  global playerlives
 
   ball_rect.y += ball_speed[1]
 
-  if ball_rect.bottom >= 590 or ball_rect.top <= 10:
+  if ball_rect.bottom >= 590:
     ball_speed[1] = -ball_speed[1]
+    playerlives -= 1
+    reset_ball()
+    
+
   if ball_rect.colliderect(paddle):
     ball_speed[1] = -ball_speed[1]
     straight = False
+
 def ball_movement_sideways():
   global ball_speed
+  global playerlives
   ball_rect.x += ball_speed[0]
   ball_rect.y += ball_speed[1]
 
-  if ball_rect.bottom >= 590 or ball_rect.top <= 10:
+  if ball_rect.bottom >= 590:
     ball_speed[1] = -ball_speed[1]
+    playerlives -= 1
+    reset_ball()
+  if paddle.x <= 300:
+    if ball_rect.top <= 10:
+     ball_speed[1] = -ball_speed[1]
 
-  if ball_rect.left <= 10 or ball_rect.right >= 790:
-    ball_speed[0] = -ball_speed[0]
+  if paddle.x <= 300:
+    if ball_rect.left <= 10 or ball_rect.right >= 790:
+      ball_speed[0] = -ball_speed[0]
 
-  if ball_rect.colliderect(paddle):
-    ball_speed[1] = -ball_speed[1]
+  if paddle.x <= 300:
+    if ball_rect.colliderect(paddle):
+      ball_speed[1] = -ball_speed[1]
+  
+  if paddle.x >= 300:
+    if ball_rect.top <= 10:
+     ball_speed[1] = ball_speed[1]
+
+  if paddle.x >= 300:
+    if ball_rect.left <= 10 or ball_rect.right >= 790:
+      ball_speed[0] = ball_speed[0]
+
+  if paddle.x >= 300:
+    if ball_rect.colliderect(paddle):
+      ball_speed[1] = ball_speed[1]
 #Border
 walls = [
   pygame.Rect(0, 0, 800, 10),  # Top horizontal wall
@@ -113,9 +153,9 @@ while running:
           if keys[pygame.K_RIGHT]:
               paddle.x -= speed
     for block in blocks:
-        if ball_rect.colliderect(block):
-            blocks.remove(block)
-            ball_speed[1] = -ball_speed[1]
+      if ball_rect.colliderect(block):
+        blocks.remove(block)
+        ball_speed[1] = -ball_speed[1]
             
 
     screen.fill(WHITE)
@@ -126,9 +166,12 @@ while running:
       ball_movement_straight()
     if straight == False:
       ball_movement_sideways()
-    lives()
-    if ball_rect.bottom > 590:
-        playerlives -= 1
+    livesText()
+    if playerlives == 0:
+      endtextLose()
+      ball_speed = [0, 0]
+    if len(blocks) == 0:
+      endtextWin()
     pygame.draw.rect(screen, BLACK, paddle)
     pygame.draw.circle(screen, BLACK, ball_rect.center, 10)
     for wall in walls:
