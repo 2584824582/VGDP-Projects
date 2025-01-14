ac5471d7f3f4ab6e29f581bc8d0ab0610def03c0
2584824582 <81043855+2584824582@users.noreply.github.com>
2025-01-14 17:52:48 -0500

    Breakout.py

--- Breakout.py
+++ Breakout.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, pickle
 
 #Initialize Pygame
 pygame.init()
@@ -9,6 +9,12 @@ playerlives = 3
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Breakout Pygame")
 
+#High Scores
+try:
+    with open('score.txt', 'rb') as file:
+        score = pickle.load(file)
+except:
+    score = 0
 #Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -18,6 +24,7 @@ YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 PURPLE = (128, 0, 128)
+#images
 Galaxy_image = pygame.image.load('Galaxy.jpg')
 blockimage = pygame.image.load('BlockImage.jpg')
 MrHaye = pygame.image.load('MRHAYE.jpg')
@@ -26,7 +33,7 @@ MrHaye = pygame.image.load('MRHAYE.jpg')
 speed = 9
 clock = pygame.time.Clock()
 paddle = pygame.Rect(350, 500, 100, 20)
-
+#highscoretxt = open('score.txt')
 #Block dimensions for Colors
 BLOCK_WIDTH = 80
 BLOCK_HEIGHT = 45
@@ -55,16 +62,55 @@ def livesText():
   text2 = font2.render(str(playerlives), True, WHITE)
   text_rect2 = text2.get_rect(center = (135, 570))
   screen.blit(text2, text_rect2)
+
 def endtextLose():
   font3 = pygame.font.Font(None, 48)
   text3 = font3.render("You Lose!", True, WHITE)
   text_rect3 = text3.get_rect(center = (395, 270))
   screen.blit(text3, text_rect3)
+
 def endtextWin():
   font4 = pygame.font.Font(None, 48)
   text4 = font4.render("You Win!!", True, WHITE)
   text_rect4 = text4.get_rect(center = (395, 270))
   screen.blit(text4, text_rect4)
+
+def Theme():
+  font5 = pygame.font.Font(None, 48)
+  text5 = font5.render("Which theme do you want?", True, BLACK)
+  text_rect5 = text5.get_rect(center = (400, 200))
+  screen.blit(text5, text_rect5)
+
+  font6 = pygame.font.Font(None, 48)
+  text6 = font6.render("Haye", True, BLACK)
+  text_rect6 = text6.get_rect(center = (250, 275))
+  screen.blit(text6, text_rect6)
+
+  font7 = pygame.font.Font(None, 48)
+  text7 = font7.render("Space", True, BLACK)
+  text_rect7 = text7.get_rect(center = (550, 275))
+  screen.blit(text7, text_rect7)
+
+def scoring():
+  font8 = pygame.font.Font(None, 48)
+  text8 = font8.render("Score:", True, WHITE)
+  text_rect8 = text8.get_rect(center = (700, 570))
+  screen.blit(text8, text_rect8)
+
+  font9 = pygame.font.Font(None, 48)
+  text9 = font9.render(str(score), True, WHITE)
+  text_rect9 = text9.get_rect(center = (775, 570))
+  screen.blit(text9, text_rect9)
+def highscore():
+  font10 = pygame.font.Font(None, 48)
+  text10 = font10.render("High Score: ", True, WHITE)
+  text_rect10 = text10.get_rect(center = (400, 400))
+  screen.blit(text10, text_rect10)
+
+  #font11 = pygame.font.Font(None, 48)
+  #text11 = font11.render(str(highscoretxt), True, WHITE)
+  #text_rect11 = text11.get_rect(center = (775, 570))
+  #screen.blit(text11, text_rect11)
 #Create the list
 for row in range(BLOCK_ROWS):
   for col in range(BLOCK_COLUMNS):
@@ -129,20 +175,38 @@ walls = [
   pygame.Rect(790, 10, 10, 600), # Right vertical wall
 ]
 #Game loops
-Themepick = True
-gamescript = False
+ThemePick = True
+game_script_space = False
+game_script_haye = False
 straight = True
 
 while ThemePick:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            Themepick = False
-    
+            ThemePick = False
+    score = 0
+    button1 = buttons[0]
+    button2 = buttons[1]
+    screen.fill(WHITE)
+    pygame.draw.rect(screen, PURPLE, button1)
+    pygame.draw.rect(screen, PURPLE, button2)
+    Theme()
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button1.collidepoint(pygame.mouse.get_pos()):
+                game_script_haye = True
+                ThemePick = False
+                break
+            elif button2.collidepoint(pygame.mouse.get_pos()):
+                game_script_space = True
+                ThemePick = False
+                break
+    pygame.display.flip()
 
-while gamescript:
+while game_script_space:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gamescript = False
+            game_script_space = False
           
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
@@ -161,14 +225,15 @@ while gamescript:
       if ball_rect.colliderect(block):
         blocks.remove(block)
         ball_speed[1] = -ball_speed[1]
+        score += 1
             
 
     screen.blit(Galaxy_image, (0, 0))
     #Draw Shapes
+    scoring()
     for block in blocks:
       pygame.draw.rect(screen, RED, block)
-      #screen.blit(blockimage, block) 
-      screen.blit(MrHaye, block)
+      screen.blit(blockimage, block)
     if straight == True:
       ball_movement_straight()
     if straight == False:
@@ -176,6 +241,7 @@ while gamescript:
     livesText()
     if playerlives == 0:
       endtextLose()
+      highscore()
       ball_speed = [0, 0]
     if len(blocks) == 0:
       endtextWin()
@@ -187,5 +253,58 @@ while gamescript:
       pygame.draw.rect(screen, WHITE, wall)
     pygame.display.flip()
     clock.tick(30)
+
+while game_script_haye:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_script_haye = False
+          
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        paddle.x -= speed
+    if keys[pygame.K_RIGHT]:
+        paddle.x += speed
+
+    #Collisions
+    for wall in walls:
+      if paddle.colliderect(wall):
+          if keys[pygame.K_LEFT]:
+              paddle.x += speed
+          if keys[pygame.K_RIGHT]:
+              paddle.x -= speed
+    for block in blocks:
+      if ball_rect.colliderect(block):
+        blocks.remove(block)
+        ball_speed[1] = -ball_speed[1]
+        score += 1
+            
+
+    screen.blit(Galaxy_image, (0, 0))
+    #Draw Shapes
+    scoring()
+    for block in blocks:
+      pygame.draw.rect(screen, RED, block) 
+      screen.blit(MrHaye, block)
+    if straight == True:
+      ball_movement_straight()
+    if straight == False:
+      ball_movement_sideways()
+    livesText()
+    if playerlives == 0:
+      endtextLose()
+      highscore()
+      ball_speed = [0, 0]
+    if len(blocks) == 0:
+      endtextWin()
+      reset_ball()
+      ball_speed [0, 0]
+    pygame.draw.rect(screen, WHITE, paddle)
+    pygame.draw.circle(screen, WHITE, ball_rect.center, 10)
+    for wall in walls:
+      pygame.draw.rect(screen, WHITE, wall)
+    pygame.display.flip()
+    clock.tick(30)
+with open('score.txt', 'wb') as file:
+    pickle.dump(score, file)
 pygame.quit()
 sys.exit()
