4b8303da2760ad477e114f0eae44a714e8c8e822
2584824582 <81043855+2584824582@users.noreply.github.com>
2025-03-03 21:06:17 -0500

    Update NHLTeams.py

--- NHLTeams.py
+++ NHLTeams.py
@@ -7,7 +7,8 @@ teams = [
     "Senators", "Flyers", "Penguins", "Sharks", 
     "Kraken", "Blues", "Lightning", "Maple_Leafs", "Hockey_Club", 
     "Canucks", "Golden_Knights", "Capitals", "Jets"]
-positions = ["C", "RW", "LW", "Def1", "Def2", "G"]
+positionsAI = ["C", "RW", "LW", "Def1", "Def2", "G"]
+positionsUser = ["C", "RW", "LW", "Def1", "Def2", "G"]
 used_players = []
 used_Positions1 = []
 used_Positions2 = []
